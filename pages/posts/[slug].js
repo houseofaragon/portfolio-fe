b8f36bbdabfd7076885bb4143a7e8e0451748842
Karen C. Aragon <saintkaren@gmail.com>
2022-03-11 12:51:23 -0500

    adding all stub pages

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -11,11 +11,16 @@ import PostTitle from '@/components/post-title'
 import Head from 'next/head'
 import { CMS_NAME } from '@/lib/constants'
 import markdownToHtml from '@/lib/markdownToHtml'
+import ReactMarkdown from 'react-markdown'
+import { rehype } from 'rehype'
+import Image from 'next/image'
 
 export default function Post({ post, morePosts, preview, error }) {
   const router = useRouter()
-  const { title, content } = post;
-  console.log('post', post)
+  console.log(post)
+
+  const { title, content, slug, img } = post;
+  const htmlContent = `<div>${content}</div>`
   return (
     <Layout preview={preview}>
         <Header />
@@ -26,12 +31,20 @@ export default function Post({ post, morePosts, preview, error }) {
           <>
               <Head>
                 <title>
-                  {post.title} | Karen Aragon
+                  {title} | Karen Aragon
                 </title>
               </Head>
               <div className="max-w-2xl mx-auto pr-10">
-                <h2 className="text-xl md:text-l font-bold">{post.title}</h2>
-                <PostBody content={post.content} />
+                <h2 className="text-xl md:text-l font-bold">{title}</h2>
+                <br/>
+                {img && img.data[0] && img.data[0]. attributes &&
+                <Image
+                  width={2000}
+                  height={1000}
+                  src={`${process.env.NEXT_PUBLIC_STRAPI_API_URL}${img.data[0]?.attributes?.url}`}
+                  alt={`Image for ${slug}`}                
+                />}
+                <PostBody content={content} />
               </div>
           </>
         )}
@@ -42,9 +55,8 @@ export default function Post({ post, morePosts, preview, error }) {
 }
 
 export async function getStaticProps({ params, preview = null }) {
-  console.log('params', params)
   const data = await getPostAndMorePosts(params.slug, preview)
-  console.log(data)
+  
   if (!data || !data.posts || !data.posts.data.length){
     return {
       props: {
@@ -58,16 +70,8 @@ export async function getStaticProps({ params, preview = null }) {
 
   const post = data?.posts?.data[0]
   const { content } = post?.attributes;
-
-  // const postContent = await Promise.all(blocks.map(async block => {
-  //   let contentToConvert = '';
-  //   if(['ComponentSharedRichText', 'ComponentSharedQuote'].includes(block.__typename) ) {
-  //     contentToConvert = block.body
-  //   }
-  //   return await markdownToHtml(content)
-  // }))
-
   const postContent =  await markdownToHtml(content)
+
   return {
     props: {
       preview,
