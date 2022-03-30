f6cce4f08471fe08d4bf4f12e7ca4619f5b08d8b
Karen C. Aragon <saintkaren@gmail.com>
2022-03-30 18:10:48 -0400

    update readme

--- components/meta.js
+++ components/meta.js
@@ -36,6 +36,7 @@ export default function Meta() {
         name="description"
         content={`Karen Aragon`}
       />
+      <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital@0;1&amp;display=swap" rel="stylesheet"></link>
       <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
